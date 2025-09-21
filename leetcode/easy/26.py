d942ad26fe037dcc50a7958b1b037ec82297aded
Arthur Hozanna <90093341+arthurhzna@users.noreply.github.com>
2025-09-21 13:56:12 +0700

    feat: solve problem 26

---
+++ leetcode/easy/26.py
@@ -0,0 +1,31 @@
+from typing import List
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        if not nums:  
+            return 0
+        i = 0  
+        for j in range(1, len(nums)):
+            if nums[j] != nums[i]:  
+                i += 1
+                nums[i] = nums[j]
+        return i + 1 
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n < 2:
+            return n
+        write = 1  
+        prev = nums[0]
+        for j in range(1, n):
+            curr = nums[j]
+            if curr != prev:         
+                nums[write] = curr
+                write += 1
+                prev = curr          
+        return write
+
+
+
+
