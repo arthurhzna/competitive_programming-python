d0d5fa323d242ec0c83caf6a94eb8550f9a76f38
Arthur Hozanna <90093341+arthurhzna@users.noreply.github.com>
2025-09-29 09:34:45 +0700

    resolve problem 7

--- leetcode/medium/7.py
+++ leetcode/medium/7.py
@@ -1,19 +1,29 @@
-class Solution:
-    def reverse(self, x: int) -> int:
-        INT_MAX = 2**31 - 1
-        INT_MIN = -2**31
-        rev = 0
-        sign = -1 if x < 0 else 1
-        x = abs(x)
-        while x != 0:
-            pop = x % 10       
-            x = x // 10        
-            if rev > (INT_MAX - pop) // 10:
-                return 0
+# class Solution:
+#     def reverse(self, x: int) -> int:
+#         INT_MAX = 2**31 - 1
+#         INT_MIN = -2**31
+#         rev = 0
+#         sign = -1 if x < 0 else 1
+#         x = abs(x)
+#         while x != 0:
+#             pop = x % 10       
+#             x = x // 10        
+#             if rev > (INT_MAX - pop) // 10:
+#                 return 0
             
-            rev = rev * 10 + pop
+#             rev = rev * 10 + pop
         
-        return sign * rev
+#         return sign * rev
 # // dapat digit terakhir % 10
 # // dapat bilangan setelah digit terkahir di hapus // 10
 # // tambahkan nilainya ke 10 100 1000 dst, yaitu di * 10 + digit terakhir
+
+class Solution:
+    def reverse(self, x: int) -> int:
+        INT_MAX, INT_MIN = 2**31 - 1, -2**31
+        sign = -1 if x < 0 else 1
+        rev = int(str(abs(x))[::-1])
+        if INT_MIN<rev<INT_MAX:
+            return sign *rev
+        else:
+            return 0
