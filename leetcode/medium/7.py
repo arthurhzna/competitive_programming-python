0f79126b22c38c5561f44b5cdd9fb7f316c09543
Arthur Hozanna <90093341+arthurhzna@users.noreply.github.com>
2025-09-29 09:15:49 +0700

    solve problem 7

---
+++ leetcode/medium/7.py
@@ -0,0 +1,19 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        INT_MAX = 2**31 - 1
+        INT_MIN = -2**31
+        rev = 0
+        sign = -1 if x < 0 else 1
+        x = abs(x)
+        while x != 0:
+            pop = x % 10       
+            x = x // 10        
+            if rev > (INT_MAX - pop) // 10:
+                return 0
+            
+            rev = rev * 10 + pop
+        
+        return sign * rev
+# // dapat digit terakhir % 10
+# // dapat bilangan setelah digit terkahir di hapus // 10
+# // tambahkan nilainya ke 10 100 1000 dst, yaitu di * 10 + digit terakhir
